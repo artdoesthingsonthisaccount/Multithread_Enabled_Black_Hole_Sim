e2858b472bd436a05698ce0bd40d6e719e3bbe95
Art Muley <asmuley2@illinois.edu>
2025-08-14 18:44:14 -0500

    a lot of learning needs to be done, including all of the physics behind this stuff. MIT OCW probably

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+#define NUM_RAYS 10 //macro for the number of Rays we want to simulate
+
+
+int main(){
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
